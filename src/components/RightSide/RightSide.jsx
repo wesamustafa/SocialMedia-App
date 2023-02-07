695621c4274453770f04a0788be0dfa31ae8de65
wesamustafa <wesamustafa@gmail.com>
2023-02-07 10:55:37 +0200

    FrontEnd-Add Trend Card

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -4,6 +4,7 @@ import Home from '../../img/home.png'
 import Noti from '../../img/noti.png'
 import Comment from '../../img/comment.png'
 import { UilSetting } from '@iconscout/react-unicons'
+import TrendCard from '../TrendCard/TrendCard'
 const RightSide = () => {
     return (
         <div className="RightSide">
@@ -13,6 +14,10 @@ const RightSide = () => {
                 <img src={Noti} alt="NOTIIcon" />
                 <img src={Comment} alt="CommentIcon" />
             </div>
+            <TrendCard />
+            <button className="button r-button">
+                Share
+            </button>
         </div>
     )
 }
