075ce63a8a9e540bfc3b489d1503465430373010
wesamustafa <wesamustafa@gmail.com>
2023-02-09 12:23:54 +0200

    RestAPI-Authentication route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import AuthRoute from './Routes/AuthRoute.js';
+
+// Routes
+
 const app = express();
+
+// Middelware
 app.use(bodyParser.json({limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb', extended:true}));
 
@@ -11,4 +17,7 @@ dotenv.config()
 
 mongoose.connect(process.env.MONGO_DB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>app.listen(process.env.PORT, ()=>console.log(`Listening at ${process.env.PORT}`)))
-.catch((error)=>console.log(error))
+.catch((error)=>console.log(error));
+
+// usages of routes
+app.use('/auth', AuthRoute);
