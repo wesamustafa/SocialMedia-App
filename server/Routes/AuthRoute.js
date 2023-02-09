5b7f04ad3346a4497a0bbc2449ac0644a22ce50d
wesamustafa <wesamustafa@gmail.com>
2023-02-09 17:39:30 +0200

    RestAPI-Auth Controller

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import { loginUser, RegisterUser } from '../Controllers/AuthController.js';
 const router = express.Router()
 
-router.get('/', async(req,res)=>{res.send("Auth Route")})
+// router.get('/', async(req,res)=>{res.send("Auth Route")})
+router.post('/register', RegisterUser)
+router.post('/login', loginUser)
 
 export default router
