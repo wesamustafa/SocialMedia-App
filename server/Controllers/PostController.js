c1a13e0c8222c96d41e096ca0666192cc8b7c1a8
wesamustafa <wesamustafa@gmail.com>
2023-02-15 14:11:45 +0200

    RestAPI- Create timeline posts

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,5 +1,6 @@
 import PostModel from "../Models/PostModel.js";
 import mongoose from "mongoose";
+import UserModel from "../Models/UserModel.js";
 
 // Create new post
 export const createPost = async(req, res)=>{
@@ -82,4 +83,39 @@ export const likePost =  async(req,res)=>{
     }
 }
 
+// Get Timeline Posts
+
+export const getTimelinePosts = async(req, res)=>{
+    const userId = req.params.id;
+    try {
+        const currentUserPost = await PostModel.find({userId: userId})
+        const followingPosts =  await UserModel.aggregate([
+            {
+                $match:{
+                    _id:new mongoose.Types.ObjectId(userId)
+                }
+            },{
+                $lookup:{
+                    from:"posts",
+                    localField:"following",
+                    foreignField:"userId",
+                    as:"followingPosts"
+                }
+            },{
+                $project:{
+                    followingPosts:1,
+                    _id:0
+                }
+            }
+        ])
+        res.status(200).json(currentUserPost.concat(...followingPosts[0].followingPosts)
+        .sort((a, b)=>{
+            return b.createdAt - a.createdAt;
+        })
+        )
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
 
