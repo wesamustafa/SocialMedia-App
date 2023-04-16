92980ccb7d37067244c80b6796e5cd197e2de08a
wesamustafa <wesamustafa@gmail.com>
2023-04-16 23:50:57 +0200

    Integration-React Router

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,15 +1,23 @@
+import { useSelector } from "react-redux";
 import "./App.css"
 import Auth from "./pages/Auth/Auth";
-// import Home from "./pages/Home/Home";
+import Home from "./pages/Home/Home";
 // import Profile from "./pages/Profile/Profile";
+import {Routes, Route, Navigate} from 'react-router-dom';
 function App() {
+  const user = useSelector((state)=>state.authReducer.authData)
   return (
     <div className="App">
         <div className="blur" style={{top:'-18%', right:'0'}}></div>
         <div className="blur" style={{top:'36%', left:'-8rem'}}></div>
         {/* <Home /> */}
         {/* <Profile /> */}
-        <Auth/>
+        {/* <Auth/> */}
+        <Routes>
+          <Route path='/' element={user?<Navigate to ="home"/>: <Navigate to= 'auth'/>} />
+          <Route path="/home" element={user? <Home/> : <Navigate to='../auth'/>} />
+          <Route path='/auth' element={user ? <Navigate to='../home'/> : <Auth/>} />
+        </Routes>
     </div>
   );
 }
