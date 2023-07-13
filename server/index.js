de7c79fed7fadd6bfacc4ffa4d2e0a103b762a41
wesamustafa <wesamustafa@gmail.com>
2023-07-14 00:26:01 +0300

    chat-API

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import AuthRoute from './Routes/AuthRoute.js';
 import UserRoute from './Routes/UserRoute.js';
 import PostRoute from './Routes/PostRoute.js';
 import UploadRoute from './Routes/UploadRoute.js'
+import MessageRoute from './Routes/MessageRoute.js'
+import ChatRoute from './Routes/ChatRoute.js'
 // Routes
 
 const app = express();
@@ -30,5 +32,7 @@ app.use('/auth', AuthRoute);
 app.use('/user', UserRoute );
 app.use('/post', PostRoute );
 app.use('/upload', UploadRoute );
+app.use("/chat", ChatRoute);
+app.use('/message', MessageRoute)
 
 
